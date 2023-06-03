482ced97a5482c3f3545c177f19915d871916368
Trinay Bhati <bhatiji780780@gmail.com>
2023-06-03 23:40:06 +0530

    3-june

--- 1-june/src/App.jsx
+++ 1-june/src/App.jsx
@@ -2,27 +2,75 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 let count1;
+let intervalId;
 
 function App() {
-  const [countDown, setCountDown] = useState(10);
+  const [countDown, setCountDown] = useState(0);
+  const [enabled, setEnabled] = useState(false);
 
-  useEffect(() => {
-    count1 = setInterval(() => {
-      setCountDown((prevState) => prevState - 1);
-    }, 1000);
+  // useEffect(() => {
+  //   count1 = setInterval(() => {
+  //     setCountDown((prevState) => prevState - 1);
+  //   }, 1000);
+  // }, []);
 
-    setTimeout(() => {
-      clearInterval(count1);
-    }, 5000);
-  }, []);
-
-  // if (countDown == 0) {
+  // if (countDown <= 0) {
   //   clearInterval(count1);
   // }
 
+  // useEffect(() => {
+  //   if (countDown !== 0) {
+  //     setTimeout(() => {
+  //       setCountDown((prevState) => prevState - 1);
+  //     }, 1000);
+  //   }
+  // }, [countDown]);
+
+  // const start = () => {
+  //   intervalId = setInterval(() => {
+  //     setCountDown((prevState) => prevState + 1);
+  //   }, 1000);
+  // };
+
+  // const stop = () => {
+  //   clearInterval(intervalId);
+  // };
+
+  // const reset = () => {
+  //   stop();
+  //   setCountDown(0);
+  // };
+
+  useEffect(() => {
+    if (enabled) {
+      intervalId = setInterval(() => {
+        setCountDown((prevState) => prevState + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [enabled]);
+
+  const start = () => {
+    setEnabled(true);
+  };
+
+  const stop = () => {
+    setEnabled(false);
+  };
+
+  const reset = () => {
+    stop();
+    setCountDown(0);
+  };
+
   return (
     <>
-      <h1>Countdown : {countDown}</h1>
+      <h1>{countDown}</h1>
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+      <button onClick={reset}>Reset</button>
     </>
   );
 }
--- 2-june/src/App.jsx
+++ 2-june/src/App.jsx
@@ -1,13 +1,111 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import "./App.css";
 
+const INITIAL_STATE = {
+  users: [],
+  loading: false,
+  error: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return { loading: true, users: [], error: "" };
+    case "SUCCESS":
+      return { loading: false, users: action.payload, error: "" };
+    case "ERROR":
+      return { loading: false, users: [], error: action.payload };
+    default:
+      return state;
+  }
+};
+
 function App() {
+  // const [users, setUsers] = useState([]);
+
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  useEffect(() => {
+    (async () => {
+      // setLoading(true);
+      dispatch({ type: "LOADING" });
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users`
+        );
+        if (!response.ok) {
+          throw new Error(`API error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        // setUsers(data);
+        // setError("");
+        dispatch({ type: "SUCCESS", payload: data });
+      } catch (e) {
+        // const errorMessage = e.message;
+        // setError(errorMessage);
+        dispatch({ type: "ERROR", payload: e.message });
+      }
+      // finally {
+      //   setLoading(false);
+      // }
+    })();
+  }, []);
+  if (state.loading) {
+    return <h3>LOADING....</h3>;
+  }
+  if (state.error) {
+    return <h3>{state.error}</h3>;
+  }
+
   return (
     <>
-      <h2>Hello</h2>
+      {state.users.map((user) => (
+        <h2>{user.name}</h2>
+      ))}
     </>
   );
 }
 
+// const INITIAL_STATE = {
+//   count: 0,
+// };
+// const reducer = (state, action) => {
+//   // if (action.type == "INCREMENT") {
+//   //   return { count: state.count + 1 };
+//   // }
+//   // if (action.type == "DECREMENT") {
+//   //   return { count: state.count - 1 };
+//   // }
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return { count: state.count + 1 };
+//     case "DECREMENT":
+//       return { count: state.count - 1 };
+//   }
+//   return state;
+// };
+
+/**function App() {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  const increment = () => {
+    const action = { type: "INCREMENT" };
+    dispatch(action);
+  };
+
+  const decrement = () => {
+    const action = { type: "DECREMENT" };
+    dispatch(action);
+  };
+
+  return (
+    <div>
+      <h2>{state.count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}**/
+
 export default App;
- 
