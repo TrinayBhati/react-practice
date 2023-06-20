bc8aa441837d7408dfebd239c57a1669d10888e5
Trinay Bhati <bhatiji780780@gmail.com>
2023-06-20 23:25:32 +0530

    20-june

--- 16-june/src/App.jsx
+++ 16-june/src/App.jsx
@@ -1,12 +1,45 @@
 import { useState } from "react";
 import "./App.css";
 
+const BUTTON_WRAPPER_STYLES = {
+  position: "relative",
+  zIndex: 1,
+};
+
+const OTHER_CONTENT_STYLES = {
+  position: "relative",
+  zIndex: 2,
+  backgroundColor: "green",
+  padding: "10px",
+};
+
+const Modal = ({ children, open, onClose }) => {
+  return (
+    <div>
+      <button onClick={onClose}>Close Modal</button>
+      {children}
+    </div>
+  );
+};
+
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div>
-        <button onClick={handleClick}></button>
+        <button style={BUTTON_WRAPPER_STYLES} onClick={() => setIsOpen(true)}>
+          Open Modal
+        </button>
+        <Modal
+          open={isOpen}
+          onClose={() => {
+            setIsOpen(false);
+          }}
+        >
+          Modal
+        </Modal>
       </div>
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
     </>
   );
 }
