50053fc7f55c4900c03202d8a78d327e8db1623f
Trinay Bhati <bhatiji780780@gmail.com>
2023-08-23 23:08:07 +0530

    23-aug

--- notes.jsx
+++ notes.jsx
@@ -58,48 +58,8 @@ React.createElemet("div", {id : "main"
     _store : {}
 }
 
-// virtual dom is a lightweight representation of dom virtual dom consists of react elements, unlike dom which is made up of html elements
-
-//=========================================================================
-
 // for node and browser working - 10th July 2023 recorded session 
 
-// react working and basics convo - https://chat.openai.com/share/0716abc7-9a6f-43bf-8e2c-390dd81f9517
-
-// 1. Virtual DOM: React uses a lightweight in-memory representation of the real web page DOM called the Virtual DOM. It tracks state changes and computes efficient updates for rendering.
-
-// 2. Components: React breaks UI into reusable pieces called components, which can be functional or class-based. Functional components return JSX, while class-based components define a render method.
-
-// 3. Rendering: React starts rendering from a top-level component, generating JSX elements to build the Virtual DOM.
-
-// 4. Reconciliation: After rendering, React compares previous and new Virtual DOMs to find minimal changes needed, enhancing performance.
-
-// 5. Updating State and Props: Components have mutable state and immutable props. Changes in either trigger re-rendering and a new reconciliation process.
-
-// 6. Component Lifecycle: Components follow a lifecycle with methods like `componentDidMount`, `componentDidUpdate`, and `componentWillUnmount` for specific actions during their lifespan.
-
-// 7. One-way Data Flow: Data flows uni-directionally, from parent to child components via props, with callbacks for child-to-parent communication.
-
-// 8. Event Handling: Events are handled declaratively by attaching handlers to JSX elements, triggering Virtual DOM updates and re-renders.
-
-// 9. Conditional Rendering: React allows dynamic UIs by conditionally rendering components or elements based on state or conditions.
-
-//=========================================================================
-
-// Babel:
-
-// - Compilation: Babel compiles modern JavaScript to older versions (ES6+ to ES5) for browser compatibility.
-// - JSX Transformation: Babel transforms JSX to regular JavaScript function calls.
-// - Plugin System: Babel's modular design uses plugins for customizable transformations.
-
-// Reconciliation Algorithm:
-
-// - Virtual DOM Diffing: React compares new Virtual DOM with the previous one.
-// - Element-Level Diffing: React fine-grainedly compares components, not just the whole tree.
-// - Keys: `key` attribute helps track changes in lists or repeated elements.
-// - Reusing Components: React reuses components for efficiency.
-// - Batched Updates: React applies multiple updates together for better performance.
-// - Priority Updates: React prioritizes crucial UI updates.
 
 //=========================================================================
 
@@ -125,3 +85,13 @@ Boolean(null) // false
   // when react jsx array it'll render them one by one
 
 //props passed only from parent to childl  
+
+//=========================================================================
+
+// function components don't have lifecycle methods but useEffect can act like them
+useEffect(()=>{},[]); // component did mount
+useEffect(()=>{}); // component did update
+useEffect(()=>{},[a]); // combination of shouldCOmponentUpdate and componentDidUpdate
+useEffect(()=>{
+    return anything; // clean-up acts as componentWillUnmount
+},[]); 
